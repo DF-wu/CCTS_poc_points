8f1120d2ed985b3c343674160a62ba48b7da0235
dfder <a2470abc@gmail.com>
2022-02-23 23:11:24 +0800

    init

---
+++ src/main/java/tw/dfder/ccts_poc_updatepoints/CctsPocUpdatepointsApplication.java
@@ -0,0 +1,13 @@
+package tw.dfder.ccts_poc_updatepoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CctsPocUpdatepointsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CctsPocUpdatepointsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tw/dfder/ccts_poc_updatepoints/CctsPocUpdatepointsApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.dfder.ccts_poc_updatepoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CctsPocUpdatepointsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
