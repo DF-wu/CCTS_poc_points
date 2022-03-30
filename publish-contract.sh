0e01baa47281aef1d0b79b17ec6782ebd980cda0
df <df@dfder.tw>
2022-03-30 23:30:12 +0800

    fix: remove unused script.

--- publish-contract.sh
+++ publish-contract.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-docker run --rm  pactfoundation/pact-cli:latest broker can-i-deploy --pacticipant 'Example App' --broker-base-url 23.dfder.tw:10141 --latest
 docker run --rm -w ${PWD} -v ${PWD}:${PWD} pactfoundation/pact-cli:latest publish ${PWD}/Contracts/pacts/  --broker-base-url http://23.dfder.tw:10141  --consumer-app-version "v0.1"
 
