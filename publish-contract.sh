cc92757ae12f3dcb347fe5b9373d70c299fe83a9
df <df@dfder.tw>
2022-03-30 17:58:28 +0800

    feat: add publish to pact broker.

---
+++ publish-contract.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker run --rm  pactfoundation/pact-cli:latest broker can-i-deploy --pacticipant 'Example App' --broker-base-url 23.dfder.tw:10141 --latest
+docker run --rm -w ${PWD} -v ${PWD}:${PWD} pactfoundation/pact-cli:latest publish --broker-base-url 23.dfder.tw:10141 ${PWD}/example/pacts --consumer-app-version "v0.1" --tag-with-git-branch
