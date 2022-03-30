1e1285101114ce5103aa7ad082a87f4c5ad974c3
df <df@dfder.tw>
2022-03-30 18:15:33 +0800

    fix: arguments format.

--- publish-contract.sh
+++ publish-contract.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker run --rm  pactfoundation/pact-cli:latest broker can-i-deploy --pacticipant 'Example App' --broker-base-url 23.dfder.tw:10141 --latest
-docker run --rm  pactfoundation/pact-cli:latest publish ${PWD}/Contracts/pacts  --broker-base-url 23.dfder.tw:10141  --consumer-app-version "v0.1" --tag-with-git-branch
+docker run --rm  pactfoundation/pact-cli:latest publish ${PWD}/Contracts/pacts  --broker-base-url 23.dfder.tw:10141  --consumer-app-version "v0.1"
